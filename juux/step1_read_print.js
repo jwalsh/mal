55117ed02efaf0986ef47a64d9d739b685aa25fb
Jason Walsh <jwalsh@booster.com>
2015-03-01 15:50:13 -0500

    Validating input processing
    Initial REPL construction available with:
    
      echo '(+ 1 1)' | node juux/step1_read_print.js
    
    This should fail on test cases run though:
    
      ./runjuuxn.sh 1

---
+++ juux/step1_read_print.js
@@ -0,0 +1,32 @@
+var READ = function(s1, s2) {
+    return s1;
+};
+
+var EVAL = function(s1, s2) {
+    return s1;
+};
+
+var PRINT = function(s1, s2) {
+    return s1;
+};
+
+var rep = function(s1) {
+    return PRINT(EVAL(READ(s1)));
+};
+
+var PROMPT = 'juux-user> ';
+
+process.stdin.setEncoding('utf8');
+process.stdout.write(PROMPT);
+
+process.stdin.on('readable', function() {
+  var chunk = process.stdin.read();
+    if (chunk !== null) {
+        process.stdout.write(rep(chunk));
+        process.stdout.write(PROMPT);
+  }
+});
+
+process.stdin.on('end', function() {
+    process.stdout.write('juux done...');
+});
