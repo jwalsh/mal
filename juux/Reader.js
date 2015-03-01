9392897eb4cb2b3c6f32314298b56f5568c4b8fa
Jason Walsh <jwalsh@booster.com>
2015-03-01 17:35:12 -0500

    Collecting the results of the RegExp
    Applying the canonical construct for reducing the RegExp into an Array.
    This will result in a structure that looks like the following:
    
    1 :
     ["1"]
    (+ 1 2) :
     ["(","+","1","2",")"]
    (+ 2 (+ 2 3)) :
     ["(","+","2","(","+","2","3",")",")"]

--- juux/Reader.js
+++ juux/Reader.js
@@ -1,5 +1,7 @@
 var exports = module.exports  = {};
 
+// var PCRE = require('pcre').PCRE;
+
 var read_str = function(str) {
     var tokens = tokenizer(str);
     var reader = new Reader(tokens);
@@ -7,13 +9,18 @@ var read_str = function(str) {
 };
 
 var tokenizer = function(str) {
-    var token_re = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/;
-    return token_re.exec(str);
+    var re_tokenizer = /[\s,]*(~@|[\[\]{}()'`~^@]|"(?:\\.|[^\\"])*"|;.*|[^\s\[\]{}('"`,;)]*)/g;
+    var match;
+    var results = [];
+    while (match = re_tokenizer.exec(str)[1]) {
+        results.push(match[0]);
+    }
+    return results;
 };
 
 var tokenizer_tests = ['1', '(+ 1 2)', '(+ 2 (+ 2 3))'];
 tokenizer_tests.map(function(e, i, c) {
-    console.log(e, tokenizer(e));
+    console.log(e, ':\n', JSON.stringify(tokenizer(e)));
 });
 
 
@@ -21,14 +28,20 @@ var read_form = function() {
 
 };
 
-var Reader = function() {
+var Reader = function(tokens) {
+    this.tokens = tokens;
+    this.position = 0;
+    var next = function() {
+        position++;
+        return peek();
+    };
+    var peek = function() {
+        return tokens[position];
+    };
+
     return {
-        next: function() {
-
-        },
-        peek: function() {
-
-        }
+        next: next,
+        peek: peek
 
     };
 };
